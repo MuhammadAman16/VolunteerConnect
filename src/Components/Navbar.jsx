efc6abc36f865da2541889094fa494e93929da80
Tahir326 <119787687+Tahir326@users.noreply.github.com>
2024-06-21 21:16:08 +0500

    Add logout button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-const Navbar = ({ toggleSidebar }) => {
+const Navbar = ({ toggleSidebar, onLogout }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleLogout = () => {
+    onLogout();
+    navigate('/login');
+  };
+
+
+  if (location.pathname === '/login') {
+    return null; 
+  }
+
   return (
     <nav className="bg-white px-6 py-4 sticky top-0 shadow-md flex justify-between items-center">
-      <div className="flex items-center  ">
+      <div className="flex items-center">
         <button className="mr-4" onClick={toggleSidebar}>
           <svg
             className="h-6 w-6"
@@ -23,7 +37,7 @@ const Navbar = ({ toggleSidebar }) => {
         <h1 className="text-2xl font-bold text-customgreen1">Volunteer Connect</h1>
       </div>
 
-
+      <button className="text-base font-semibold bg-customgreen1 px-3 py-2 rounded-3xl text-white  hover:text-gray-900" onClick={handleLogout}>Logout</button>
     </nav>
   );
 };
